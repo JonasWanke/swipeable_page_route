1ebe0a5e4ea73f583144106fe1785bebaccfb893
Jonas Wanke <contact@jonas-wanke.com>
2023-01-25 12:35:06 +0100

    Add dart_code_metrics

--- lib/swipeable_page_route.dart
+++ lib/swipeable_page_route.dart
@@ -2,6 +2,6 @@ export 'src/app_bar/app_bar.dart' show MorphingAppBar;
 export 'src/app_bar/sliver_app_bar.dart' show MorphingSliverAppBar;
 export 'src/page_route.dart'
     show
+        BuildContextSwipeablePageRoute,
         SwipeablePageRoute,
-        SwipeablePageTransitionsBuilder,
-        BuildContextSwipeablePageRoute;
+        SwipeablePageTransitionsBuilder;
