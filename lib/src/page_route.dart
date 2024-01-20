9cee84a1c12e50f91c46c0a2487d155f5183fe6e
Jonas Wanke <contact@jonas-wanke.com>
2024-01-20 19:49:25 +0100

    Handle new pageRoute.popDisposition

--- lib/src/page_route.dart
+++ lib/src/page_route.dart
@@ -163,12 +163,9 @@ class SwipeablePageRoute<T> extends CupertinoPageRoute<T> {
     if (route.willHandlePopInternally) return false;
     // If attempts to dismiss this route might be vetoed such as in a page
     // with forms, then do not allow the user to dismiss the route with a swipe.
-    //
-    // TODO(JonasWanke): Use `route.hasScopedWillPopCallback ||
-    // route.popDisposition == RoutePopDisposition.doNotPop` after upgrading
-    // Flutter.
     // ignore: deprecated_member_use
-    if (route.hasScopedWillPopCallback) return false;
+    if (route.hasScopedWillPopCallback ||
+        route.popDisposition == RoutePopDisposition.doNotPop) return false;
     // Fullscreen dialogs aren't dismissible by back swipe.
     if (route.fullscreenDialog) return false;
     // If we're in an animation already, we cannot be manually swiped.
