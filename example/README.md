d10f4c4432342d1744fe94a4e0bb516c61c38078
Jonas Wanke <contact@jonas-wanke.com>
2024-12-18 19:11:06 +0100

    Regenerate Android platform

--- example/README.md
+++
@@ -1,3 +0,0 @@
-# 🔙 swipeable_page_route_example
-
-Example for 🔙 swipeable_page_route, a package adding back-navigation by swiping with beautifully morphing widgets
--- example/android/app/src/main/kotlin/dev/wanke/swipeable_page_route_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/dev/wanke/swipeable_page_route_example/MainActivity.kt
@@ -2,5 +2,4 @@ package dev.wanke.swipeable_page_route_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
