0b50afadcd55e21d3ce1660a3e5d0ef94d38bce0
Jonas Wanke <contact@jonas-wanke.com>
2024-12-18 19:18:19 +0100

    Remove dependency on flutter_color_models
    Closes: #79. That package is incompatible with Flutter 3.27 and was not yet updated.

--- lib/src/app_bar/state.dart
+++ lib/src/app_bar/state.dart
@@ -1,9 +1,9 @@
+import 'dart:math' as math;
 import 'dart:ui';
 
 import 'package:black_hole_flutter/black_hole_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_color_models/flutter_color_models.dart';
 
 import 'leading.dart';
 
@@ -81,11 +81,108 @@ class MorphingState {
   static Color _lerpColor(Color? a, Color? b, double t) {
     if (a == null && b == null) return Colors.transparent;
 
-    final aAsOklab = a == null ? null : OklabColor.fromColor(a);
-    final bAsOklab = b == null ? null : OklabColor.fromColor(b);
-    return (aAsOklab ?? bAsOklab!.withAlpha(0))
-        .interpolate(bAsOklab ?? aAsOklab!.withAlpha(0), t)
-        .toColor();
+    return _OklabColor.lerp(
+          a == null ? null : _OklabColor.fromRgb(a),
+          b == null ? null : _OklabColor.fromRgb(b),
+          t,
+        )?.toRgb() ??
+        Colors.transparent;
+  }
+}
+
+extension type _OklabColor(
+    ({double alpha, double l, double a, double b}) components) {
+  factory _OklabColor.fromRgb(Color color) {
+    final r = _srgbComponentToLinear(color.r);
+    final g = _srgbComponentToLinear(color.g);
+    final b = _srgbComponentToLinear(color.b);
+
+    // https://bottosson.github.io/posts/oklab/#converting-from-linear-srgb-to-oklab
+
+    final l = 0.4122214708 * r + 0.5363325363 * g + 0.0514459929 * b;
+    final m = 0.2119034982 * r + 0.6806995451 * g + 0.1073969566 * b;
+    final s = 0.0883024619 * r + 0.2817188376 * g + 0.6299787005 * b;
+
+    final l_ = math.pow(l, 1 / 3);
+    final m_ = math.pow(m, 1 / 3);
+    final s_ = math.pow(s, 1 / 3);
+
+    return _OklabColor(
+      (
+        alpha: color.a,
+        // ignore: double-literal-format
+        l: 0.2104542553 * l_ + 0.7936177850 * m_ - 0.0040720468 * s_,
+        // ignore: double-literal-format
+        a: 1.9779984951 * l_ - 2.4285922050 * m_ + 0.4505937099 * s_,
+        // ignore: double-literal-format
+        b: 0.0259040371 * l_ + 0.7827717662 * m_ - 0.8086757660 * s_,
+      ),
+    );
+  }
+
+  static double _srgbComponentToLinear(double component) {
+    // https://bottosson.github.io/posts/colorwrong/#what-can-we-do%3F
+    return component >= 0.0031308
+        ? (1.055 * math.pow(component, 1.0 / 2.4)) - 0.055
+        : 12.92 * component;
+  }
+
+  static _OklabColor? lerp(_OklabColor? x, _OklabColor? y, double t) {
+    return switch ((x, y)) {
+      (null, null) => null,
+      (final x?, null) => x.scaleAlpha(1 - t),
+      (null, final y?) => y.scaleAlpha(t),
+      (final x?, final y?) => _OklabColor(
+          (
+            alpha: lerpDouble(x.components.alpha, y.components.alpha, t)!,
+            l: lerpDouble(x.components.l, y.components.l, t)!,
+            a: lerpDouble(x.components.a, y.components.a, t)!,
+            b: lerpDouble(x.components.b, y.components.b, t)!,
+          ),
+        ),
+    };
+  }
+
+  _OklabColor scaleAlpha(double factor) =>
+      withAlpha(clampDouble(components.alpha * factor, 0, 1));
+  _OklabColor withAlpha(double alpha) {
+    return _OklabColor(
+      (alpha: alpha, l: components.l, a: components.a, b: components.b),
+    );
+  }
+
+  Color toRgb() {
+    final c = components;
+
+    // https://bottosson.github.io/posts/oklab/#converting-from-linear-srgb-to-oklab
+
+    final l_ = c.l + 0.3963377774 * c.a + 0.2158037573 * c.b;
+    final m_ = c.l - 0.1055613458 * c.a - 0.0638541728 * c.b;
+    // ignore: double-literal-format
+    final s_ = c.l - 0.0894841775 * c.a - 1.2914855480 * c.b;
+
+    final l = l_ * l_ * l_;
+    final m = m_ * m_ * m_;
+    final s = s_ * s_ * s_;
+
+    final r = 4.0767416621 * l - 3.3077115913 * m + 0.2309699292 * s;
+    final g = -1.2684380046 * l + 2.6097574011 * m - 0.3413193965 * s;
+    // ignore: double-literal-format
+    final b = -0.0041960863 * l - 0.7034186147 * m + 1.7076147010 * s;
+
+    return Color.from(
+      alpha: components.alpha,
+      red: _srgbComponentFromLinear(r),
+      green: _srgbComponentFromLinear(g),
+      blue: _srgbComponentFromLinear(b),
+    );
+  }
+
+  static double _srgbComponentFromLinear(double component) {
+    // https://bottosson.github.io/posts/colorwrong/#what-can-we-do%3F
+    return (component >= 0.04045)
+        ? math.pow((component + 0.055) / (1 + 0.055), 2.4).toDouble()
+        : component / 12.92;
   }
 }
 
