2193354fd59eccd76038e0eebfcf13c40d6b39df
Jonas Wanke <contact@jonas-wanke.com>
2026-01-02 12:53:38 +0100

    Regenerate Android platform directory

---
+++ example/android/app/src/main/kotlin/dev/wanke/swipeable_page_route/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.wanke.swipeable_page_route.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- example/android/app/src/main/kotlin/dev/wanke/swipeable_page_route_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dev.wanke.swipeable_page_route_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
